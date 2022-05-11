0dae4f5299cf7dc04a3e7fb4d35b23e585d7b2a1
abdohisham1998 <31512324+abdohisham1998@users.noreply.github.com>
2022-05-11 02:39:53 +0200

    asd

---
+++ js/min.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        items:4,
+        loop:true,
+        margin:1,
+        autoplay:true,
+        autoplayTimeout:5000,
+        animateOut: 'fadeOut',
+        autoplayHoverPause:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+    })
+  });
